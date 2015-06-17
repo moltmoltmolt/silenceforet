2012ddc89e180b1e24d961610c46784227084257
moltmoltmolt <mathilde.fenoll@gmail.com>
2015-06-16 22:31:47 -0700

    Update menu.js

--- meta/menu.js
+++ meta/menu.js
@@ -1,13 +1,8 @@
 var menu = " \
     <div id=\"menu\" data-markdown> \
-    Silence Fôret
       #### LE COLLECTIF \
-      [à propos](/texte/texte.htm) \
+      [Info](/texte/texte.htm) \
       <br/> \
-      #### EXPOSITIONS \
-      * [Castel(/malacologie1.htm) \
-      #### RESIDENCES \
-      * [Malacologie #1](/malacologie1.htm) \
       * [contact](/contact.htm) \
     </div> \
 ";
