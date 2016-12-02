0d7499d2096f5cb0b5f5c475e538bbac161d5372
Mathilde Fenoll <mathilde.fenoll@gmail.com>
2016-12-02 15:35:42 +0100

    changes on Boubou

--- meta/menu.js
+++ meta/menu.js
@@ -14,6 +14,8 @@ var menu = " \
       <br/> \
       [Castel #3](/castel-3.htm) \
       <br/> \
+      [Cream of Trust](/cream.htm) \
+      <br/> \
       [M/C/I](/mci.htm) \
       <br/> \
       [Ici-Bas](/icibas.htm) \
