b377bee9b6a888f1c87c605183ce83b20a478686
Mathilde Fenoll <mathilde.fenoll@gmail.com>
2018-04-09 21:37:25 +0200

    changes on Boubou

--- meta/menu.js
+++ meta/menu.js
@@ -3,22 +3,20 @@ var menu = " \
       [SILENCE FORÊT](/index.htm) \
       <br/> \
       <br/> \
-      [à propos](/about.htm) \
+      [/Who/What](/about.htm) \
       <br/> \
       [contact](/contact.htm) \
       <br/> \
       <br/> \
-      [Castel #1](/castel-1.htm) \
-      <br/> \
-      [Castel #2](/castel-2.htm) \
-      <br/> \
-      [Castel #3](/castel-3.htm) \
-      <br/> \
-      [Cream](/cream.htm) \
+      [Ici-Bas](/icibas.htm) \
       <br/> \
       [M/C/I](/mci.htm) \
       <br/> \
-      [Ici-Bas](/icibas.htm) \
+      [Cream](/cream.htm) \
+      <br/> \
+      [Castel #2](/castel-2.htm) \
+      <br/> \
+      [Castel #1](/castel-1.htm) \
       <br/> \
     </div> \
 ";
