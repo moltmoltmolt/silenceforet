eae0c0e19778f536f437b918616ec18d3dfd8ecb
Mathilde Fenoll <mathilde.fenoll@gmail.com>
2018-05-28 18:35:44 +0200

    changes on Boubou

--- meta/menu.js
+++ meta/menu.js
@@ -1,25 +1,25 @@
-var menu = " \
-    <div id=\"menu\" data-markdown> \
-      [SILENCE FORÊT](/index.htm) \
-      <br/> \
-      <br/> \
-      [.about](/about.htm) \
-      <br/> \
-      <br/> \
-      <br/> \
-      [Castel#1](/castel-1.htm) \
-      <br/> \
-      [Castel#2](/castel-2.htm) \
-      <br/> \
-      [Cream](/cream.htm) \
-      <br/> \
-      [M/C/I](/mci.htm) \
-      <br/> \
-      [Ici-Bas](/icibas.htm) \
-      <br/> \
-      <br/> \
-      [R.A.V.E.](/rave.htm) \
-      <br/> \
-      <br/> \
-      </div> \
-";
+var menu = `
+    <div id="menu" data-markdown>
+      [SILENCE FORÊT](/index.htm)
+      <br/>
+      <br/>
+      [.about](/about.htm)
+      <br/>
+      <br/>
+      <br/>
+      [Castel#1](/castel-1.htm)
+      <br/>
+      [Castel#2](/castel-2.htm)
+      <br/>
+      [Cream](/cream.htm)
+      <br/>
+      [M/C/I](/mci.htm)
+      <br/>
+      [Ici-Bas](/icibas.htm)
+      <br/>
+      <br/>
+      [R.A.V.E.](/rave.htm)
+      <br/>
+      <br/>
+    </div>
+`;
