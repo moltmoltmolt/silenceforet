12a5a271bcc3a1623c43961d48ce1b075e6d37fb
Mathilde Fenoll <mathilde.fenoll@gmail.com>
2020-01-02 16:20:28 +0100

    changes on Boubou

--- meta/menu.js
+++ meta/menu.js
@@ -8,8 +8,6 @@ var menu = `
       <br/>
       [prints](/editions.htm)
       <br/>
-      [chez Nout](/nout.htm)
-      <br/>
       [Castel#1](/castel-1.htm)
       <br/>
       [Castel#2](/castel-2.htm)
@@ -23,6 +21,8 @@ var menu = `
       [R.A.V.E.](/rave.htm)
       <br/>
       [the Lodge](/lodge.htm)
+      <br/>
+      [chez Nout](/nout.htm)
       <br/>
       <br/>
       <br/>
